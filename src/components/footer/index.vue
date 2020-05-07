e0abf9d5fea3d60eb2dd7d364ae9c7004607a0f4
Lionel Francis <lezardvaldth@gmail.com>
2020-05-07 00:28:56 -0500

    updates

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -6,7 +6,7 @@
       <div class="col-lg-3 mb-5 mb-lg-0">
         <a class="d-inline-flex align-items-center mb-2" href="index.html" aria-label="Front">
       <!--  -->
-      <img src="@/assets/automarket.png" width="200px">
+      <img src="@/assets/automarket-white.png" width="200px">
         </a>
       </div>
 
@@ -29,33 +29,35 @@
 
       <div class="col-lg-5">
         <!-- Form -->
-        <form class="js-validate mb-2">
-          <h4 class="h6 text-white font-weight-medium mb-3">Stay up to date</h4>
-          <div class="form-row">
-            <div class="col-9">
-              <div class="js-form-message">
-                <label class="sr-only" for="subscribeSrEmail">Email address</label>
-                <div class="input-group">
-                  <input type="email" class="form-control" name="email" id="subscribeSrEmail" placeholder="Email address" aria-label="Email address" required
-                         data-msg="Please enter a valid email address."
-                         data-error-class="u-has-error"
-                         data-success-class="u-has-success">
-                </div>
-              </div>
-            </div>
+         <h4 class="h6 font-weight-semi-bold mb-4">We are driven to deliver results for all your businesses.</h4>
 
-            <div class="col-3">
-              <button type="submit" class="btn btn-primary">
-                <span class="fas fa-arrow-right"></span>
-              </button>
-            </div>
-          </div>
-        </form>
+            <!-- Button -->
+            <button type="button" class="btn btn-xs btn-primary btn-wide transition-3d-hover text-left mb-2 mr-1">
+              <span class="media align-items-center">
+                <span class="fab fa-apple fa-2x mr-3"></span>
+                <span class="media-body">
+                  <span class="d-block">Download on the</span>
+                  <strong class="font-size-1">App Store</strong>
+                </span>
+              </span>
+            </button>
+            <!-- End Button -->
+
+            <!-- Button -->
+            <a type="button" href="https://play.google.com/store/apps/details?id=com.automarket.app" class="btn btn-xs btn-primary btn-wide transition-3d-hover text-left mb-2">
+              <span class="media align-items-center">
+                <span class="fab fa-google-play fa-2x mr-3"></span>
+                <span class="media-body">
+                  <span class="d-block">Get it on</span>
+                  <strong class="font-size-1">Google Play</strong>
+                </span>
+              </span>
+            </a>
         <!-- End Form -->
       </div>
     </div>
 
-    <p class="small text-white-70 mb-0">© 2020 automarketjm. All rights reserved.</p>
+    <p class="small text-white-70 mb-0">© 2020 automarketjm.com All rights reserved.</p>
   </div>
 </footer>
 <!-- ========== END FOOTER ========== -->
